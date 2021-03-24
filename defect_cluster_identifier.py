ad58ab3c26e03eaa6f5edfb607f18889ff03b591
ssk1991 <kumar256@purdue.edu>
2021-03-23 22:42:56 -0700

    Modify identify_clusters if condition to be DRY

--- defect_cluster_identifier.py
+++ defect_cluster_identifier.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 import pandas as pd
 import argparse
+from collections import defaultdict
 
 
 # TODO: Move utils into separate file
@@ -55,10 +56,10 @@ def defect_codes_to_analyze(category="all"):
         "IGL",
         "IGJ",
     ]
-    hole_codes = ["HSV", "HVV"]
+    hole_codes = ["H", "HSV", "HVV"]
     fracture_codes = ["FL", "FC", "FM", "FS", "FH", "FH2", "FH3", "FH4"]
     crack_codes = ["CL", "CC", "CM", "CS", "CH", "CH2", "CH3", "CH4"]
-    broken_codes = ["BSV", "BVV"]
+    broken_codes = ["B", "BSV", "BVV"]
     collapse_codes = ["X"]
 
     tap_codes = [
@@ -168,6 +169,7 @@ def get_pacp_grade(defect_code):
         "JAM": 1,
         "JAL": 2,
         "X": 5,
+        "B": 4,
         "BSV": 5,
         "BVV": 5,
         "DR": 5,
@@ -177,6 +179,7 @@ def get_pacp_grade(defect_code):
         "DFE": 5,
         "DTBR": 5,
         "DTBI": 5,
+        "H": 4,
         "HSV": 5,
         "HVV": 5,
         "FL": 3,
@@ -200,14 +203,14 @@ def get_pacp_grade(defect_code):
         print("Defect code not in dict")
 
 
-def filter_category(df_cond, keep_defects):
+def filter_df_by_defects(df_cond, keep_defects):
     """Delete defects that are not in keep_defects"""
     print(
         f"Total number of inspections to begin with are {format(df_cond['InspectionID'].nunique())}"
     )
     df_cond = df_cond[df_cond["PACP_Code"].isin(keep_defects)]
     print(
-        f"Number of inspections that with defects under consideration are: {df_cond['InspectionID'].nunique()}"
+        f"Number of inspections with defects under consideration are: {df_cond['InspectionID'].nunique()}"
     )
 
     return df_cond
@@ -230,16 +233,18 @@ def identify_clusters_in_single_inspection(df_cond, cluster_dist_thresh, insp_id
 
     for index in indices:
         defect_curr = df_temp.at[index, "PACP_Code"]  # Defect code at current index
+        cond_id = df_temp.at[index, "ConditionID"]  # ConditionID at current index
+        video_frame = df_temp.at[index, "Counter"]  # Frame at current index
+
         dist_curr = float(
             df_temp.at[index, "Distance"]
         )  # Distance of defect at current index
 
-        if abs(dist_curr - dist_prev) < cluster_dist_thresh:
-            cluster_curr.append((insp_id, defect_curr, dist_curr))
-        else:
+        if abs(dist_curr - dist_prev) >= cluster_dist_thresh:
             clusters.append(cluster_curr)
             cluster_curr = []
-            cluster_curr.append((insp_id, defect_curr, dist_curr))
+
+        cluster_curr.append((insp_id, defect_curr, dist_curr, cond_id, video_frame))
 
         dist_prev = dist_curr
         defect_prev = defect_curr
@@ -251,8 +256,11 @@ def identify_clusters_in_multiple_inspections(df_cond, cluster_dist_thresh):
     clusters = []
     insp_ids = df_cond["InspectionID"].unique()
 
+    # Loop through all inspections
     for insp_id in insp_ids:
+        # Get df corresponding to a particular inspection
         df_cond_single_inspection = df_cond[df_cond["InspectionID"] == insp_id]
+        # Identify clusters in inspection and add to list of clusters
         clusters.extend(
             identify_clusters_in_single_inspection(
                 df_cond_single_inspection, cluster_dist_thresh, insp_id
@@ -265,11 +273,7 @@ def identify_clusters_in_multiple_inspections(df_cond, cluster_dist_thresh):
 
 
 def calc_num_clusters(clusters):
-    cluster_len = [len(i) for i in clusters]  # List of cluster lengths
-    max_cluster_len = max(cluster_len)
-
-    num_clusters = {i: 0 for i in range(1, max_cluster_len + 1)}
-    max_length = 0
+    num_clusters = defaultdict(int)
     for cluster in clusters:
         if len(cluster) >= 1:
             num_clusters[len(cluster)] += 1
@@ -281,7 +285,7 @@ def calc_cluster_severity(cluster, len_thresh=3):
     cluster_length = 0
     grade = 0
 
-    for _, defect_code, _ in cluster:
+    for _, defect_code, _, _, _ in cluster:
         grade += get_pacp_grade(defect_code)
 
     num_defects = len(cluster)
@@ -300,7 +304,7 @@ def filter_clusters(clusters, num_defects_thresh, severity_thresh):
     filtered_clusters = [
         cluster
         for cluster in clusters
-        if len(cluster) == num_defects_thresh
+        if len(cluster) >= num_defects_thresh
         and calc_cluster_severity(cluster)[0] > severity_thresh
     ]
     return filtered_clusters
@@ -320,9 +324,9 @@ def parse_args():
 
 
 def ipynb_fake_args(
-    cond_db="data/PACP_databases/Conditions_Combined.csv",
+    cond_db="data/Condition_Databases/Conditions_SAI.csv",
     defect_category="structural",
-    cluster_dist_thresh=3,
+    cluster_dist_thresh=6.0,
 ):
     """
     cluster_dist_thresh: maximum distance between 2 defects to consider them in a cluster
@@ -339,30 +343,34 @@ def ipynb_fake_args(
 
 
 def main():
+    # main
     is_notebook = True
     if not is_notebook:
         args = parse_args()
     else:
         args = ipynb_fake_args()
 
+    # Read csv containing single or multiple inspections
     df_cond = pd.read_csv(args.cond_db, sep=",")
+
+    # Get list of defect codes to keep
     keep_defects = defect_codes_to_analyze(args.defect_category)
-    df_cond = filter_category(df_cond, keep_defects)
 
-    clusters = identify_clusters_in_multiple_inspections(df_cond, int(args.thresh))
+    # Filter the df to only keep particular code
+    df_cond = filter_df_by_defects(df_cond, keep_defects)
 
-    num_clusters, max_cluster_len = calc_num_clusters(clusters)
-    # cluster_metrics = calc_cluster_metrics(clusters, int(args.thresh), max_cluster_len)
+    # Identify clusters
+    clusters = identify_clusters_in_multiple_inspections(
+        df_cond, int(args.cluster_dist_thresh)
+    )
 
     filtered_clusters = filter_clusters(
         clusters, num_defects_thresh=3, severity_thresh=1
     )
 
-    # # Save cluster metrics as a json
-    # with open("cluster_metrics.json", "w") as f:
-    #     json.dump(cluster_metrics, f)
-
-    # TODO: Visualize defect cluster metrics
+    # Calculate number of clusters
+    num_clusters, max_cluster_len = calc_num_clusters(filtered_clusters)
+    print(f"Number of clusters of different sizes is: {num_clusters}")
 
 
 if __name__ == "__main__":
